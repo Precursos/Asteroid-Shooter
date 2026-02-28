09c1169bcd04ab921f437cdc294de0c806afe2a8
Precursos Kaijuhead <143108922+Precursos@users.noreply.github.com>
2026-02-27 22:03:08 -0600

    Configuración de velocidad de asteroides y de rotacion

--- scenes/asteroids/big_asteroid.gd
+++ scenes/asteroids/big_asteroid.gd
@@ -1,10 +1,23 @@
 extends Area2D
 
-@export var speed:float
+@export var min_speed: float
+@export var max_speed: float
+
+@export var min_rotation_speed: float
+@export var max_rotation_speed: float
+
+var random_speed
+var random_speed_rotation
 
 func _ready():
-	pass
+	random_speed = randf_range(min_speed, max_speed)
+	random_speed_rotation = randf_range(min_rotation_speed, max_rotation_speed)
 
+
+#Leer documetación.
+#Delta es el tiempo entre frames por segundo
 
 func _process(delta):
 	position.x -= random_speed * delta
+	#Si en vez de poner un "+" se pone un "-" la rotación se invierte
+	rotation_degrees += random_speed_rotation * delta
